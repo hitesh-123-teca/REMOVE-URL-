3cdc9901f486eeb78de196951234e750553413ab
hiteshrathiya <hiteshrathiya123@gmail.com>
2025-12-06 09:31:54 +0530

    Delete db.py

--- db.py
+++
@@ -1,8 +0,0 @@
-from pymongo import MongoClient
-from config import MONGODB_URI
-
-client = MongoClient(MONGODB_URI)
-db = client["telebot_db"]
-forwards_col = db["forwards"]
-state_col = db["state"]
-dup_col = db["duplicates"]
